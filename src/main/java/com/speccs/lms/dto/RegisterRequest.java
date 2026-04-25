3c273bdc0376506a315c79973f803bb3cadee229
Sureekutty <jsureendhar480@gmail.com>
2026-04-25 23:06:02 +0530

    feat: add auth controller with login and register endpoints

---
+++ src/main/java/com/speccs/lms/dto/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.speccs.lms.dto;
+
+import lombok.Data;
+import java.util.Set;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+    private Set<String> roles;
+}
